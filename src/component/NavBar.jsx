15330069fe0ebc95bf40a55e37ed9ca5d4126401
rameeezz <ramezhossam92@gmail.com>
2024-08-03 23:03:49 +0300

    ........

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -4,7 +4,8 @@ import { NavLink, Link } from 'react-router-dom'
 export default function NavBar() {
   return (
     <>
-   <nav className='position-fixed z-2 bg-black w-100'>
+   <nav className='position-fixed z-2 bg-transparent w-100 text-primary d-flex py-3 gap-5'>
+    <NavLink to='home'>Home</NavLink>
    <NavLink to='register' className={  (isActive)=> isActive? "nav-link active":"nav-link"}>Register</NavLink>
    <NavLink to='login' className={  (isActive)=> isActive? "nav-link active":"nav-link"}>login</NavLink>
    </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import akPhoto from '../../public/akIcon.jpg'
 export default function Home() {
   return (
-    <div>sayed</div>
+    <>
+    <div className='w-100 height  d-flex justify-content-center align-items-center'>
+    <img src={akPhoto} alt="sss" className='w-50 hiegt' />
+    </div>
+    </>
   )
 }
