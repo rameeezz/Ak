3e137018a02726ba957f05c8e6a391a6c74b405a
rameeezz <ramezhossam92@gmail.com>
2024-08-03 23:14:08 +0300

    .......

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import akPhoto from '../../public/akIcon.jpg'
 export default function Home() {
   return (
     <>
-    <div className='w-100 height  d-flex justify-content-center align-items-center'>
+    <div className='w-100 h-vh  d-flex justify-content-center align-items-center'>
     <img src={akPhoto} alt="sss" className='w-50 hiegt' />
     </div>
     </>
