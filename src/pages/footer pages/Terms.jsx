1260671a2109f721998c95a4913e3e79a212ee39
rameeezz <ramezhossam92@gmail.com>
2024-08-06 08:32:16 +0300

    ...

--- src/pages/footer pages/Terms.jsx
+++ src/pages/footer pages/Terms.jsx
@@ -4,6 +4,11 @@ export default function Terms() {
   return (
     <>
      <p>terms</p>
+     <div className="d-flex justify-content-center align-items-center my-5">
+        <button>
+          <i className="fa solid fa-spinner fa-spin textSize"></i>
+        </button>
+      </div>
     </>
   )
 }
