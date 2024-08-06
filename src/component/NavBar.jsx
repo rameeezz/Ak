fda0504cc19e63482c7d46a17df632253fea3d77
rameeezz <ramezhossam92@gmail.com>
2024-08-06 07:56:57 +0300

    ......

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "../css/NavBar.css";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useNavigate } from "react-router-dom";
 export default function NavBar({ user, logOut }) {
+  let navigate = useNavigate()
+  function goOutt() {
+    navigate("/admin1")
+  }
   return (
     <>
    
-      {user?.role == "admin1" ? "ssssssss" : <nav className="position-fixed z-3 bg-transparent w-100 text-primary d-flex py-3 gap-5">
+      {user?.role == "admin1" ? "" : <nav className="position-fixed z-3 bg-transparent w-100 text-primary d-flex py-3 gap-5">
         <NavLink to="home">Home</NavLink>
         {user != null ? (
           <button onClick={logOut}>log out</button>
@@ -30,6 +34,7 @@ export default function NavBar({ user, logOut }) {
           </div>
         )}
       </nav>}
+      <button onClick={goOutt}> sssssssssssssss</button>
     </>
   );
 }
