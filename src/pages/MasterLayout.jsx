100935885081ff6ffeb0b3fe3bb1bb1dc650af66
rameeezz <ramezhossam92@gmail.com>
2024-08-11 07:44:08 +0300

    done

--- src/pages/MasterLayout.jsx
+++ src/pages/MasterLayout.jsx
@@ -1,16 +1,33 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./../component/NavBar";
-import TransitionWrapper from "../routes/TransitionWrapper.jsx"; 
-import { Footer } from './../component/Footer';
-export default function MasterLayout({user , logOut}) {
+import TransitionWrapper from "../routes/TransitionWrapper.jsx";
+import { Footer } from "./../component/Footer";
+export default function MasterLayout({ user, logOut }) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user?.role === "admin1") {
+      navigate("/admin1");
+    }
+
+    if (user?.role === "admin2") {
+      navigate("/admin1");
+    }
+
+    if (user?.role === "customer") {
+      navigate("/admin1");
+    }
+    if (user?.role === null) {
+      navigate("/home");
+    }
+  }, [user, navigate]);
   return (
     <>
-      <NavBar user={user} logOut ={ logOut} />
+      <NavBar user={user} logOut={logOut} />
       <TransitionWrapper>
-        <Outlet /> 
+        <Outlet />
       </TransitionWrapper>
-      <Footer user={user}/>
+      <Footer user={user} />
     </>
   );
 }
