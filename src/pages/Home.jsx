fa932f3176caa43b9cad2a4b4b13fdcaf29d75f9
rameeezz <ramezhossam92@gmail.com>
2024-08-12 23:27:58 +0300

    .....

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import '../css/Home.css'
+import "../css/Home.css";
+import PHoto1 from '../assets/card photo/1.jpeg'
+import PHoto2 from '../assets/card photo/2.jpeg'
 export default function Home({ user }) {
   console.log(user);
 
@@ -21,6 +23,34 @@ export default function Home({ user }) {
         <button>
           <i className="fa solid fa-spinner fa-spin textSize"></i>
         </button>
+      </div>
+      <div className="container d-flex justify-content-center gap-4 my-5">
+      <div className="card sa" >
+        <img src={PHoto2} className="card-img-top" alt="" />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+      <div className="card sa" >
+        <img src={PHoto1} className="card-img-top" alt="" />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
       </div>
       <div className="d-flex justify-content-center align-items-center">
         <button onClick={addToCart} className="btn btn-primary p-4 ">
