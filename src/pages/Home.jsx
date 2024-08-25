c20b96b490a5bd5ae77a98433209bb1f8c6667d1
rameeezz <ramezhossam92@gmail.com>
2024-08-25 23:07:02 +0300

    ....

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,33 @@ export default function Home({ user }) {
   return (
     <>
     <img src={bg} alt="Ak Florist" className="classForBg"/>
-      <div className="container d-flex justify-content-center gap-4 my-5">
+      <div className="container d-flex justify-content-center flex-wrap gap-4 my-5">
+      <div className="card widthOfHomeCard" >
+        <img src={PHoto1} className="card-img-top" alt="" />
+        <div className="card-body">
+        <p className="text-muted mb-2">690 EGP</p>
+          <h5 className="card-title">Red Roses Bouquet</h5>
+          <p className="card-text mb-2">
+          Red Love Bouquet | Black Wrap.
+          </p>
+          <button onClick={addToCart} className="btn text-white ColorButton  w-100">
+            add to cart 
+          </button>
+        </div>
+      </div>
+      <div className="card widthOfHomeCard" >
+        <img src={PHoto1} className="card-img-top" alt="" />
+        <div className="card-body">
+        <p className="text-muted mb-2">690 EGP</p>
+          <h5 className="card-title">Red Roses Bouquet</h5>
+          <p className="card-text mb-2">
+          Red Love Bouquet | Black Wrap.
+          </p>
+          <button onClick={addToCart} className="btn text-white ColorButton  w-100">
+            add to cart 
+          </button>
+        </div>
+      </div>
       <div className="card widthOfHomeCard" >
         <img src={PHoto1} className="card-img-top" alt="" />
         <div className="card-body">
