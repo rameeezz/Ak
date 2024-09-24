e9a25c603423a7cabcaa9f37af25396f88b0684d
rameeezz <ramezhossam92@gmail.com>
2024-09-24 21:21:51 +0300

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -900,7 +900,7 @@ export default function Home({ user }) {
               </div>
               <div className="d-flex align-items-center gap-2">
                 <img src={whatsSvg} alt="" />
-                <a href="https://api.whatsapp.com/send?phone=201022317881" className="text-primary">Whatsapp</a>
+                <a href="https://api.whatsapp.com/send?phone=201022317881" className="text-primary" target="_blank">Whatsapp</a>
 
               </div>
               <div className="d-flex align-items-center gap-2">
