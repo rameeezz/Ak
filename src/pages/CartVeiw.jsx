e022803788fc31846bf5cf4a9494df524208d50f
rameeezz <ramezhossam92@gmail.com>
2024-10-01 17:29:22 +0300

    ....

--- src/pages/CartVeiw.jsx
+++ src/pages/CartVeiw.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CartVeiw() {
+export default function CartVeiw({ user }) {
+  const customerID =
+    user?.role == "customer" ? user?.userId || null : user?.id || null;
+  const [userCart, setuserCart] = useState([]);
+  const [loadingCart, setLoadingCart] = useState(false);
+  async function getOrders() {
+    try {
+      let { data } = await axios.get(
+        `https://akflorist-production.up.railway.app/customer/getOrder/${customerID}`
+      );
+      console.log(data.getThisCart);
+    } catch (error) {}
+  }
+  useEffect(() => {
+    if (customerID) {
+      getOrders();
+    }
+  }, [customerID]);
   return (
-    <div>CartVeiw</div>
-  )
+    <>
+      <div></div>
+    </>
+  );
 }
