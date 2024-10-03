685e977825ae4f55b0cfbb35023d06888c93bb84
rameeezz <ramezhossam92@gmail.com>
2024-10-03 18:16:46 +0300

    text of name  in all category page

--- src/pages/AllCategory.jsx
+++ src/pages/AllCategory.jsx
@@ -90,7 +90,7 @@ export default function AllCategory({user}) {
                   className="w-100 h-100 rounded"
                 />
                 <div className="position-absolute top-0 h-100 w-100 bg-black opacity-25 z-1 rounded"></div>
-                <div className="d-flex justify-content-center align-items-center position-absolute w-100 h-100 z-3 top-0 styleForCategories z-2">
+                <div className="d-flex justify-content-center align-items-center position-absolute w-100 h-100 z-3 top-0 responsiveTextForCategory styleForCategories z-2">
                   <h2 className="text-white text-center">{element?.name}</h2>
                 </div>
               </div>
