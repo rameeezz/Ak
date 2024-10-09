6f6201205461a49ba15ee07c46a12d036575cae9
rameeezz <ramezhossam92@gmail.com>
2024-10-10 00:18:51 +0300

    ..

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import NavBar from './../component/NavBar';
-
+import "../css/ErrorPage.css"
 export default function NotFound() {
   return (
     <>
     <NavBar/>
-    <h1 className='pt-5'>el72naay </h1>
+    <div className="error-page-container">
+      <h1 className="error-heading">Oops! Something went wrong.</h1>
+      <p className="error-message">
+        The page you're looking for doesn't exist or an error occurred.
+      </p>
+      <Link to="/" className="home-button">
+        Go to Home
+      </Link>
+    </div>
     </>
   )
 }
